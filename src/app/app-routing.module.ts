1ef21101bb69604bfac1eaca26ce334a7c029edb
Kenel Cruz <kenelcruz@gmail.com>
2021-12-25 11:56:38 -0400

    Finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuestionComponent } from './Components/question/question.component';
 import {MainScreenComponent} from './View/main-screen/main-screen.component';
-import {GameOverComponent} from './View/game-over/game-over.component';
+
 const routes: Routes = [
   {path: '', redirectTo:'main', pathMatch:'full'},
   {path: 'main', component: MainScreenComponent},
   {path: 'question', component: QuestionComponent},
-  {path: 'GameOver', component: GameOverComponent},
-  {path: 'GameOver/:puntaje', component: GameOverComponent},
+
 ];
 
 @NgModule({
@@ -16,4 +15,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routinComponents = [MainScreenComponent, QuestionComponent, GameOverComponent]
+export const routinComponents = [MainScreenComponent, QuestionComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule, routinComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './Components/question/question.component';
 import { MainScreenComponent } from './View/main-screen/main-screen.component';
-import { GameOverComponent } from './View/game-over/game-over.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +14,6 @@ import { GameOverComponent } from './View/game-over/game-over.component';
     QuestionComponent,
     routinComponents,
     MainScreenComponent,
-    GameOverComponent
   ],
   imports: [
     BrowserModule,
